60a57cb7d12a3fd45f0761693c78f1cbba156aa1
Zoé Garbal <zoe.garbal@theodo.com>
2026-04-30 10:29:57 +0200

    Introducing Lists to make code scalable

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,15 +1,30 @@
 import styles from "./Home.module.css"
 import { Pokemon } from "../../components/Pokemon"
 
+const pokemonList = [
+  {
+    name: "Carapuce",
+    id: 7,
+  },
+  {
+    name: "Carabaffe",
+    id: 8,
+  },
+  {
+    name: "Tortank",
+    id: 9,
+  },
+]
+
 export const Home = () => {
   return (
     <div className={styles.intro}>
       <div>Bienvenue sur ton futur pokédex !</div>
       <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
       <div style={{ display: "flex", gap: "1rem" }}>
-        <Pokemon name="Carapuce" id={7} />
-        <Pokemon name="Carabaffe" id={8} />
-        <Pokemon name="Tortank" id={9} />
+        {pokemonList.map(({ name, id }) => (
+          <Pokemon key={id} name={name} id={id} />
+        ))}
       </div>
     </div>
   )
