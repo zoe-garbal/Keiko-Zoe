6b386a978e18da3ddcf6789630672da23cae8335
Zoé Garbal <zoe.garbal@theodo.com>
2026-04-30 09:58:26 +0200

    Introducing PokemonProps

---
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -0,0 +1,19 @@
+interface PokemonProps {
+  name: string
+  id: number
+}
+
+export const Pokemon = ({ name, id }: PokemonProps) => {
+  const image = {
+    src: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+    alt: name,
+  }
+
+  return (
+    <div>
+      <img src={image.src} alt={image.alt} />
+      <p>Name: {name}</p>
+      <p>Number: {id}</p>
+    </div>
+  )
+}
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,32 +1,15 @@
 import styles from "./Home.module.css"
-
-interface Pokemon {
-  name: string
-  number: number
-  image: {
-    src: string
-    alt: string
-  }
-}
+import { Pokemon } from "../../components/Pokemon"
 
 export const Home = () => {
-  const pokemon: Pokemon = {
-    name: "Carapuce",
-    number: 7,
-    image: {
-      src: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
-      alt: "Carapuce",
-    },
-  }
-
   return (
     <div className={styles.intro}>
       <div>Bienvenue sur ton futur pokédex !</div>
       <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
-      <div className={styles.pokemon}>
-        <img src={pokemon.image.src} alt={pokemon.image.alt} />
-        <p>Name: {pokemon.name}</p>
-        <p>Number: {pokemon.number}</p>
+      <div style={{ display: "flex", gap: "1rem" }}>
+        <Pokemon name="Carapuce" id={7} />
+        <Pokemon name="Carabaffe" id={8} />
+        <Pokemon name="Tortank" id={9} />
       </div>
     </div>
   )
